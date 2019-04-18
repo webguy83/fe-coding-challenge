f8bd58749442c49e5510c63b8e48dd88315e6ce2
webguy83 <curtisyacboski@gmail.com>
2019-04-18 00:08:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./css/main.css";
+import "./css/form.css";
+import "./css/card.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="header">
+          <h1 className="header__content">User Roles Management</h1>
         </header>
+        <section className="control-panel">
+          <form action="#" className="control-panel__form">
+            <div className="search-group">
+              <input type="text" className="search" name="search" placeholder="search" />
+              <i className="fas fa-search"></i>
+            </div>
+            <div className="role-status-container">
+              <label htmlFor="role-status" className="role-status-label">Role Status</label>
+              <select name="role-status" className="role-status-combobox">
+                <option value="active-inactive">Active and Inactive</option>
+                <option value="active">Active</option>
+                <option value="inactive">Inactive</option>
+              </select>
+            </div>
+          </form>
+          <button className="create-role-btn btn">Create New Role</button>
+        </section>
+        <main className="main">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </main>
       </div>
     );
   }
